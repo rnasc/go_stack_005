4c18322a11be1d82413127e95f4f343b52c89780
Rogerio Nascimento <rogerio@rogerionascimento.com>
2020-11-02 17:04:09 +0000

    Exercise delivery

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,11 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +19,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (accumulator, transaction) =>
+        // eslint-disable-next-line eqeqeq
+        transaction.type == 'income'
+          ? accumulator + transaction.value
+          : accumulator,
+      0,
+    );
+    const outcome = this.transactions.reduce(
+      (accumulator, transaction) =>
+        // eslint-disable-next-line eqeqeq
+        transaction.type == 'outcome'
+          ? accumulator + transaction.value
+          : accumulator,
+      0,
+    );
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
